354680d79f9b75dde6c2c163f67e7bd3996d47af
buckldav <runalldayandnight@gmail.com>
2019-07-16 10:45:49 -0600

    https

--- src/json/students.js
+++ src/json/students.js
@@ -1,6 +1,8 @@
 const students = {
-  "Brighton Cox": {title: "Pokemon", src: "http://brighton-cox.buckldav.repl.run"},
-  "Kaleb Spencer": {title: "Hangman", src: "http://kaleb-spencer.buckldav.repl.run/"},
+  "Brighton Cox": {title: "Pokemon", src: "https://tinyurl.com/y6rvg62y"},
+  "Jacob Jones": {title: "Emoji Generator", src: "https://tinyurl.com/y5obu33d"},
+  "Kaleb Spencer": {title: "Hangman", src: "https://tinyurl.com/y69yfmnd"},
+  "Rebekah Schlutheiss": {title: "Chutes and Ladders", src: "https://tinyurl.com/y3nha4ma"},
 }
 
 export default students;
