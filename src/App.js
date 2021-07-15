f0f5160aa1f83bb45aa0bc7e0c818b284ebb2946
buckldav <runalldayandnight@gmail.com>
2021-07-15 17:24:26 -0600

    2021 projects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './main.css';
 import students from './json/students';
+import students2021 from './json/students2021'
 
 class App extends React.Component {
   state = {
+    year: "2021"
   }
 
   componentDidMount() {
@@ -12,22 +14,59 @@ class App extends React.Component {
   render() {
     return (
       <main>
-        <h1>UVU Prep CS Projects 2019</h1>
-        {this.state.name ? <h3>{this.state.name} - {students[this.state.name].title}</h3> : <h3>Check out the cool stuff we made this year!</h3>}
+        <h1>UVU Prep CS Projects 2021</h1>
+        {this.state.name ? <h3>{this.state.name}</h3> : <h3>Check out the cool stuff we made this year! Each project is the student's own work!</h3>}
         <nav className="nav">
           <ul>
             <li>
               <span className="clickable">Select Student &#9759;</span>
               <ul>
-                {Object.keys(students).map(el => (
+                {Object.keys(students2021).map(el => (
                   <li className="clickable" onClick={(e) => {this.setState({name: e.target.innerHTML})}}>{el}</li>
                 ))}
               </ul>
             </li>
           </ul>
         </nav>
-        {this.state.name ? <iframe title={students[this.state.name].title} src={students[this.state.name].src} width={800} height={500}><script>document.write()</script></iframe> : <></>}
-        <p>Each project made was the student's original work. They coded it in Python.</p>
+        
+        {this.state.name ? 
+          this.state.year === "2021" ? 
+            <>
+              {students2021[this.state.name].map(val => {
+                if (val === "micro:bit") {
+                  return <>
+                    <h4>Check out my micro:bit project in this room!</h4>
+                    <br/>
+                  </>
+                } else if (val.includes("replit.com")) {
+                  return <>
+                    <iframe title={this.state.name} width={"100%"} height={500} src={val.replace(/#.*/g, "")+"?embed=true"}></iframe>
+                    <a href={val} target="_blank" rel="noopener noreferrer">Link to project</a>
+                    <br/>
+                  </>
+                } else if (val.includes("http")) {
+                  return <>
+                    <iframe title={this.state.name} width={"100%"} height={600} src={val}></iframe>
+                    <a href={val} target="_blank" rel="noopener noreferrer">Link to project</a>
+                    <br/>
+                  </>
+                } else {
+                  return <>{val}<br/></>
+                }
+              })}
+            </> :
+          this.state.year === "2019" ? 
+            <iframe 
+              title={students[this.state.name].title} 
+              src={students[this.state.name].src} 
+              width={800} 
+              height={500}
+            >
+              <script>document.write()</script>
+            </iframe> : 
+            <></> : 
+          <></>
+        }
       </main>
     );
   }
