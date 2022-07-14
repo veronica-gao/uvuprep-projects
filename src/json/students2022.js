73957c515f95124a7c9c4fa86e7a3b91f98184cb
David <runalldayandnight@gmail.com>
2022-07-14 11:38:11 -0600

    Update students2022.js

--- src/json/students2022.js
+++ src/json/students2022.js
@@ -1,15 +1,14 @@
 const students = {
-  "Hailey G": ["https://CYOA-p5.haileygrimes.repl.co"],
-  "David B": [
-    "https://replit.com/@buckldav/DinoGameP5Starter",
-    "https://replit.com/@buckldav/PizzaStarter",
-  ],
   "Ty N": ["https://2vs5qo.csb.app/"],
   "Atticus R": ["https://final-project-cs.mckellhardman.repl.co"],
   "Abby H": ["https://replit.com/@Abbers/CYOA-p5#script.js"],
   "Ayden W": ["https://Actual-Final-CS-Project-Attemt-4.awoof07.repl.co"],
   "Devin H, Ethan A": ["https://DinosaurDE.ethanapsley1.repl.co"],
-   "Ben B" : ["https://replit.com/@BenBoucher/Circular-Jungle"],
+  "Ben B": ["https://replit.com/@BenBoucher/Circular-Jungle"],
+  "Shantel G": ["https://Jarorld-the-Block-Friend.hamyyystar.repl.co"],
+  "McKell H": ["https://final-project-cs.mckellhardman.repl.co"],
+  "Alan M": ["https://replit.com/@Jibusentanna/DinoGameP5Starter-3?from=notifications#enemy.js"],
+  "Gibson T": ["https://replit.com/@Jibusentanna/DinoGameP5Starter-3?from=notifications#enemy.js"],
 };
 
 const studentsSorted = Object.keys(students)
